1135f2e0dacc9dbf0b28cfebb0bb06b9a7ec2136
Soham Chowdhury <chow.soham@gmail.com>
2017-05-10 17:35:21 +0530

    Add defintions for common categories

--- library/Example.hs
+++ library/Example.hs
@@ -2,7 +2,11 @@
 -- | An example module.
 module Example (main) where
 
-import           Text.PrettyPrint
+import           Data.Monoid                 ((<>))
+import           Text.PrettyPrint            hiding ((<>))
+
+import           Control.Monad.Identity
+import           Control.Monad.Writer.Strict
 
 backed :: Doc -> Doc
 backed d = "\\" <> d
@@ -10,12 +14,16 @@ backed d = "\\" <> d
 unary :: Doc -> Doc -> Doc
 unary c arg = backed (c <> braces arg)
 
-newcommand1 :: Doc -> Doc -> Doc
-newcommand1 name expansion =
+newcommand0 :: Doc -> Doc -> Doc
+newcommand0 name expansion =
   backed "newcommand" <> braces (backed name) <> braces expansion
 
+declareMathOperator :: Doc -> Doc -> Doc
+declareMathOperator name expansion =
+  backed "DeclareMathOperator" <> braces (backed name) <> braces expansion
+
 scriptMacro :: Doc -> Doc -> Doc -> Doc -> Doc
-scriptMacro script pre sym letter = newcommand1 (pre <> sym) (unary script letter)
+scriptMacro script pre sym letter = newcommand0 (pre <> sym) (unary script letter)
 
 scriptMacros
   :: [(Doc, Doc)] -- ^ The symbols to process, with intended output
@@ -91,36 +99,146 @@ joinSets a b = a $$ "" $$ b
 -- Actual symbol sets
 ----------------------------------------------------------
 
-fraktur :: Doc
-fraktur = joinSets frakturUpper frakturLower
-  where
-    frakturUpper = upperSet ["i"] "mathfrak" "f"
-    frakturLower = lowerSet' "mathfrak" "f"
+singles =
+  [ frakturSingles
+  , mathscrSingles
+  , blackboardBoldSingles
+  , sansSerifSingles
+  , mathcalSingles
+  ]
+    where
+    frakturSingles = joinSets frakturUpper frakturLower
+      where
+        frakturUpper = upperSet ["i"] "mathfrak" "f"
+        frakturLower = lowerSet' "mathfrak" "f"
 
-mathscr :: Doc
-mathscr = joinSets mathscrUpper mathscrLower
-  where
-    mathscrUpper = upperSet' "mathscr" "k"
-    mathscrLower = lowerSet' "mathscr" "k"
+    mathscrSingles = joinSets mathscrUpper mathscrLower
+      where
+        mathscrUpper = upperSet' "mathscr" "k"
+        mathscrLower = lowerSet' "mathscr" "k"
 
-blackboardBold :: Doc
-blackboardBold = bbUpper
-  where
-    bbUpper = upperSet ["f"] "mathbb" "b"
+    blackboardBoldSingles = upperSet ["f"] "mathbb" "b"
 
-sansSerif :: Doc
-sansSerif = joinSets sfUpper sfLower
-  where
-    sfUpper = upperSet ["m", "q"] "sf" "r"
-    sfLower = lowerSet' "sf" "r"
+    sansSerifSingles = joinSets sfUpper sfLower
+      where
+        sfUpper = upperSet ["m", "q"] "sf" "r"
+        sfLower = lowerSet' "sf" "r"
 
-mathcal :: Doc
-mathcal = joinSets mathcalUpper mathcalLower
+    mathcalSingles = joinSets mathcalUpper mathcalLower
+      where
+        mathcalUpper = upperSet ["t"] "mathcal" "h"
+        mathcalLower = lowerSet' "mathcal" "h"
+
+
+mathrmify :: Doc -> Doc
+mathrmify = braces . unary "mathrm"
+
+hat :: Doc -> Doc
+hat = braces . unary "hat"
+
+gamma' :: Doc
+gamma' = braces $ backed "Gamma"
+
+gamma :: Doc
+gamma = braces $ backed "gamma"
+
+caret :: Doc
+caret = "^"
+
+data Newcmd0 = Newcmd0 Doc Doc Doc
+
+subscript :: Doc -> Doc -> Doc
+subscript l r = braces l <> "_" <> braces r
+
+mathcal :: Doc -> Doc
+mathcal = unary "mathcal"
+
+bold :: Doc -> Doc
+bold = unary "mathbb"
+
+sans :: Doc -> Doc
+sans = unary "mathsf"
+
+roman :: Doc -> Doc
+roman = unary "mathrm"
+
+(.-) :: Doc -> Doc -> Doc
+(.-) = subscript
+
+($=) :: Doc -> Doc -> (Doc, Doc)
+($=) = (,)
+
+infixr 4 $=
+
+(&=) :: Doc -> (Doc, Doc) -> Writer [Doc] ()
+(&=) comment (name, expansion) = tell
+      ["%%%" <+> comment $$ newcommand0 name expansion $$ ""]
+
+infixr 3 &=
+
+(!=) :: Doc -> (Doc, Doc) -> Writer [Doc] ()
+(!=) comment (name, expansion) = tell
+      ["%%%" <+> comment $$ declareMathOperator name expansion $$ ""]
+
+infixr 3 !=
+
+withHeader :: Doc -> Writer [Doc] a -> Doc
+withHeader title pairs =
+  vcat $
+  "%%%" : "%%%" <+> title : "%%%" : "" : (runIdentity . execWriterT $ pairs)
+
+algebraicNumberTheory :: Doc
+algebraicNumberTheory =
+  withHeader "Algebraic number theory" $ do
+    "Absolute Galois group of Q"           &= "agqab"
+      $= hcat [gamma', caret, mathrmify "ab"]
+    "Class group of a number field"        &= "clk"
+      $= unary "Cl" "K"
+    "Ring of p-integers in a number field" &= "op"
+      $= mathcal "O" .- backed "fP"
+    "Profinite completion of Z"            &= "zhat"
+      $= hat (bold "Z")
+
+comment :: Doc -> Writer [Doc] ()
+comment c = tell ["%%%" <+> c $$ ""]
+
+general :: Doc
+general =
+  withHeader "General commands" $ do
+    comment "Categories"
+
+    "rings"             &&= "ring" $$= "Ring"
+    "groups"            &&= "grp"  $$= "Grp"
+    "abelian groups"    &&= "ab"   $$= "Ab"
+    "sets"              &&= "set"  $$= "Set"
+    "simplicial sets"   &&= "sset" $$= "sSet"
+    "cosimplicial sets" &&= "cset" $$= "cSet"
+
+    comment "Algebraic operations"
+
+    "Cokernel"      != "coker" $= "coker"
+    "Kernel"        != "kker"  $= "ker"
+    "Automorphisms" != "aut"   $= "Aut"
+    "Endomorphisms" != "eend"  $= "End"
+
   where
-    mathcalUpper = upperSet ["t"] "mathcal" "h"
-    mathcalLower = lowerSet' "mathcal" "h"
+    infixr 3 &&=
+    (&&=) c x = "The category of" <+> c &= x
+
+    infixr 3 $$=
+    ($$=) x y = x  $= sans y
+
+
+topics :: [Doc]
+topics = [general, algebraicNumberTheory]
+
+defs :: Doc
+defs =
+  vcat $
+  punctuate
+    "\n" $ concat [singles, topics]
 
 -- | An example function.
 main :: IO ()
 main =
-  print $ vcat $ punctuate "\n" [fraktur, mathscr, blackboardBold, sansSerif, mathcal]
+  print defs
